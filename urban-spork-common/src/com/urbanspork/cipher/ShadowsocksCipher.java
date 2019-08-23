dfac67cb82a829acb369a81e45d7942602a9c184
Zmax0 <zhousha104@gmail.com>
2019-08-23 10:14:20 +0800

    cipher update

--- urban-spork-common/src/com/urbanspork/cipher/ShadowsocksCipher.java
+++ urban-spork-common/src/com/urbanspork/cipher/ShadowsocksCipher.java
@@ -2,10 +2,18 @@ package com.urbanspork.cipher;
 
 public interface ShadowsocksCipher {
 
-    byte[] encrypt(byte[] in, ShadowsocksKey key) throws Exception;
+    Cipher encrypter();
 
-    byte[] decrypt(byte[] in, ShadowsocksKey key) throws Exception;
+    Cipher decrypter();
 
     int getKeyLength();
+
+    default byte[] encrypt(byte[] in, ShadowsocksKey key) throws Exception {
+        return encrypter().encrypt(in, key.getEncoded());
+    }
+
+    default byte[] decrypt(byte[] in, ShadowsocksKey key) throws Exception {
+        return decrypter().decrypt(in, key.getEncoded());
+    }
 
 }
