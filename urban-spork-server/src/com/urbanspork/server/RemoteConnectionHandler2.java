d80969d808a9423063a2fd62ae6e11e81817f7bd
Zmax0 <zhousha104@gmail.com>
2022-10-14 19:39:43 +0800

    upgrade 2022.1

---
+++ urban-spork-server/src/com/urbanspork/server/RemoteConnectionHandler2.java
@@ -0,0 +1,59 @@
+package com.urbanspork.server;
+
+import com.urbanspork.common.channel.ChannelCloseUtils;
+import com.urbanspork.common.channel.DefaultChannelInboundHandler;
+import com.urbanspork.common.protocol.ShadowsocksProtocol;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.concurrent.FutureListener;
+import io.netty.util.concurrent.Promise;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+
+public class RemoteConnectionHandler2 extends ChannelInboundHandlerAdapter implements ShadowsocksProtocol {
+
+    private static final Logger logger = LoggerFactory.getLogger(RemoteConnectionHandler2.class);
+
+    private final Bootstrap b = new Bootstrap();
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        Channel localChannel = ctx.channel();
+        final InetSocketAddress remoteAddress = decodeAddress((ByteBuf) msg);
+        Promise<Channel> promise = ctx.executor().newPromise();
+        promise.addListener(
+                (FutureListener<Channel>) future -> {
+                    final Channel outboundChannel = future.getNow();
+                    outboundChannel.writeAndFlush(msg).addListener(outboundFuture -> {
+                                ctx.pipeline().addLast(new DefaultChannelInboundHandler(outboundChannel));
+                                outboundChannel.pipeline().addLast(new DefaultChannelInboundHandler(localChannel));
+                                if (ctx.pipeline().get(RemoteConnectionHandler2.class) != null) {
+                                    ctx.pipeline().remove(RemoteConnectionHandler2.this);
+                                }
+                            }
+                    );
+                });
+        b.group(localChannel.eventLoop())
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handler(new RemotePromiseHandler(promise))
+                .connect(remoteAddress).addListener((ChannelFutureListener) future -> {
+                    if (future.isSuccess()) {
+                        logger.info("Connect success [id: {}, L: {} - R: /{}]", localChannel.id(), localChannel.localAddress(), remoteAddress.getHostName());
+                    } else {
+                        logger.error("Connect proxy server {} failed", remoteAddress);
+                        ChannelCloseUtils.closeOnFlush(localChannel);
+                    }
+                });
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ChannelCloseUtils.closeOnFlush(ctx.channel());
+    }
+}
