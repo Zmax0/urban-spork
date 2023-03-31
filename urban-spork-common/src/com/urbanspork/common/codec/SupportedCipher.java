20fccd9145a4f46ccde4ec8bd681cb427f4f9ab9
Zmax0 <zhousha104@gmail.com>
2023-03-31 15:50:22 +0800

    Use popular cipher names

--- urban-spork-common/src/com/urbanspork/common/codec/SupportedCipher.java
+++ urban-spork-common/src/com/urbanspork/common/codec/SupportedCipher.java
@@ -1,16 +1,23 @@
 package com.urbanspork.common.codec;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum SupportedCipher {
 
-    @JsonProperty("aes-128-gcm")
     aes_128_gcm,
-    @JsonProperty("aes-192-gcm")
     aes_192_gcm,
-    @JsonProperty("aes-256-gcm")
     aes_256_gcm,
-    @JsonProperty("chacha20-poly1305")
-    chacha20_poly1305
+    chacha20_poly1305;
 
+    private final String value;
+
+    SupportedCipher() {
+        value = name().replace('_', '-');
+    }
+
+    @JsonValue
+    @Override
+    public String toString() {
+        return value;
+    }
 }
