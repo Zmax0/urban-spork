978ad5f079bea7dce0ca89deae4543b84d31ea31
Zmax0 <zhousha104@gmail.com>
2023-04-06 20:16:50 +0800

    Fix server bash startup issue

--- urban-spork-server/resource/startup.sh
+++ urban-spork-server/resource/startup.sh
@@ -6,6 +6,8 @@ if [ "${pid}" ]; then
   kill -15 "${pid}"
   sleep 1
 fi
+mkdir -p logs
+touch logs/server.log
 nohup "$JAVA_HOME"/bin/java --add-opens "java.base/jdk.internal.misc=ALL-UNNAMED" --add-opens "java.base/java.nio=ALL-UNNAMED" -Dio.netty.tryReflectionSetAccessible="true" -Dio.netty.leakDetectionLevel="SIMPLE" -Dio.netty.leakDetection.targetRecords="1" -XX:MaxDirectMemorySize="16m" -Xms64m -Xmx256m -jar "${server}.jar" >/dev/null 2>&1 &
 eval pid="$(pgrep -f ${server})"
 echo "start ${server} pid ${pid}"
