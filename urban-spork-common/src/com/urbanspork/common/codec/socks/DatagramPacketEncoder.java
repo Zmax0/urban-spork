c5900bfcb667ed258e722ee5dabb5b5fac7862ce
Zmax0 <zhousha104@gmail.com>
2024-01-19 15:46:39 +0800

    refactor(socks): socks address codec

--- urban-spork-common/src/com/urbanspork/common/codec/socks/DatagramPacketEncoder.java
+++ urban-spork-common/src/com/urbanspork/common/codec/socks/DatagramPacketEncoder.java
@@ -6,7 +6,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.handler.codec.MessageToMessageEncoder;
-import io.netty.handler.codec.socksx.v5.Socks5CommandType;
 
 import java.util.List;
 
@@ -16,7 +15,7 @@ public class DatagramPacketEncoder extends MessageToMessageEncoder<TernaryDatagr
         ByteBuf buffer = ctx.alloc().buffer();
         buffer.writeBytes(new byte[]{0, 0, 0/* fragment */});
         DatagramPacket data = msg.packet();
-        Address.encode(Socks5CommandType.CONNECT, data.recipient(), buffer);
+        Address.encode(data.recipient(), buffer);
         buffer.writeBytes(data.content());
         out.add(new DatagramPacket(buffer, msg.third(), data.sender()));
     }
--- urban-spork-common/src/com/urbanspork/common/protocol/socks/Address.java
+++ urban-spork-common/src/com/urbanspork/common/protocol/socks/Address.java
@@ -3,15 +3,35 @@ package com.urbanspork.common.protocol.socks;
 import io.netty.buffer.ByteBuf;
 import io.netty.handler.codec.DecoderException;
 import io.netty.handler.codec.EncoderException;
-import io.netty.handler.codec.socksx.v5.*;
+import io.netty.handler.codec.socksx.v5.Socks5AddressEncoder;
+import io.netty.handler.codec.socksx.v5.Socks5AddressType;
+import io.netty.handler.codec.socksx.v5.Socks5CommandRequest;
+import io.netty.util.CharsetUtil;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 public interface Address {
 
-    static void encode(Socks5CommandType type, InetSocketAddress address, ByteBuf out) {
-        encode(Socks5.toCommandRequest(type, address), out);
+    static void encode(InetSocketAddress address, ByteBuf out) {
+        if (address.isUnresolved()) {
+            String hostName = address.getHostName();
+            out.writeByte(Socks5AddressType.DOMAIN.byteValue());
+            out.writeByte(hostName.length());
+            out.writeCharSequence(hostName, CharsetUtil.US_ASCII);
+        } else {
+            InetAddress ipAddress = address.getAddress();
+            if (ipAddress instanceof Inet4Address) {
+                out.writeByte(Socks5AddressType.IPv4.byteValue());
+            } else {
+                out.writeByte(Socks5AddressType.IPv6.byteValue());
+            }
+            out.writeBytes(ipAddress.getAddress());
+        }
+        out.writeShort(address.getPort());
     }
 
     static void encode(Socks5CommandRequest request, ByteBuf out) {
@@ -31,16 +51,35 @@ public interface Address {
 
     static InetSocketAddress decode(ByteBuf in) {
         Socks5AddressType addressType = Socks5AddressType.valueOf(in.readByte());
-        String hostname;
-        try {
-            hostname = Socks5AddressDecoder.DEFAULT.decodeAddress(addressType, in);
-        } catch (Exception e) {
-            throw new DecoderException(e);
-        }
-        if (addressType == Socks5AddressType.DOMAIN) {
-            return InetSocketAddress.createUnresolved(hostname, in.readUnsignedShort());
+        if (addressType == Socks5AddressType.IPv4) {
+            byte[] bytes = new byte[4];
+            in.readBytes(bytes);
+            InetAddress addr = null;
+            try {
+                addr = InetAddress.getByAddress(bytes);
+            } catch (UnknownHostException ignore) {
+                // should never be caught
+            }
+            int port = in.readUnsignedShort();
+            return new InetSocketAddress(addr, port);
+        } else if (addressType == Socks5AddressType.IPv6) {
+            byte[] bytes = new byte[16];
+            in.readBytes(bytes);
+            InetAddress addr = null;
+            try {
+                addr = InetAddress.getByAddress(bytes);
+            } catch (UnknownHostException ignore) {
+                // should never be caught
+            }
+            int port = in.readUnsignedShort();
+            return new InetSocketAddress(addr, port);
+        } else if (addressType == Socks5AddressType.DOMAIN) {
+            int length = in.readUnsignedByte();
+            String host = in.readCharSequence(length, CharsetUtil.US_ASCII).toString();
+            int port = in.readUnsignedShort();
+            return InetSocketAddress.createUnresolved(host, port);
         } else {
-            return new InetSocketAddress(hostname, in.readUnsignedShort());
+            throw new DecoderException("unsupported address type: " + (addressType.byteValue() & 0xFF));
         }
     }
 
@@ -54,6 +93,19 @@ public interface Address {
             return 1 + 1 + request.dstAddr().length() + 2;
         } else {
             throw new UnsupportedOperationException();
+        }
+    }
+
+    static int getLength(InetSocketAddress address) {
+        if (address.isUnresolved()) {
+            return 1 + 1 + address.getHostName().length() + 2;
+        } else {
+            InetAddress ipAddress = address.getAddress();
+            if (ipAddress instanceof Inet4Address) {
+                return 1 + 4 + 2;
+            } else {
+                return 1 + 8 * 2 + 2;
+            }
         }
     }
 }
