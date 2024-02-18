93d56c4d0677a6698e515db51cd27f498c722c06
Zmax0 <zhousha104@gmail.com>
2024-02-18 11:19:29 +0800

    refactor: remove `Keys.toString()`

--- urban-spork-common/src/com/urbanspork/common/codec/shadowsocks/Keys.java
+++ urban-spork-common/src/com/urbanspork/common/codec/shadowsocks/Keys.java
@@ -4,10 +4,6 @@ import com.urbanspork.common.codec.CipherKind;
 import com.urbanspork.common.protocol.shadowsocks.aead.AEAD;
 import com.urbanspork.common.protocol.shadowsocks.aead2022.AEAD2022;
 
-import java.util.Base64;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public record Keys(byte[] encKey, byte[][] identityKeys) {
     public static Keys from(CipherKind kind, String password) {
         int keySize = kind.keySize();
@@ -23,12 +19,5 @@ public record Keys(byte[] encKey, byte[][] identityKeys) {
             throw new IllegalArgumentException(msg);
         }
         return keys;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("EK:%s, IK:%s",
-            Base64.getEncoder().encodeToString(encKey),
-            Stream.of(identityKeys).map(Base64.getEncoder()::encodeToString).collect(Collectors.joining("|")));
     }
 }
