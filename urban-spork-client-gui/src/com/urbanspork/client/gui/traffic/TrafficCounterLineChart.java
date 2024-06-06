91cc2963c679a69662a3ff7659093f3681f6bf71
Zmax0 <zhousha104@gmail.com>
2024-06-06 20:17:03 +0800

    feat(gui): use curves instead of straight lines

--- urban-spork-client-gui/src/com/urbanspork/client/gui/traffic/TrafficCounterLineChart.java
+++ urban-spork-client-gui/src/com/urbanspork/client/gui/traffic/TrafficCounterLineChart.java
@@ -1,15 +1,20 @@
 package com.urbanspork.client.gui.traffic;
 
 import com.urbanspork.client.Client;
+import com.urbanspork.client.gui.spine.CatmullRom;
 import io.netty.handler.traffic.TrafficCounter;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
+import javafx.geometry.Point2D;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.PathElement;
 import javafx.util.Duration;
 
 public class TrafficCounterLineChart {
@@ -27,7 +32,49 @@ public class TrafficCounterLineChart {
     public LineChart<Number, Number> init() {
         NumberAxis xAxis = new NumberAxis(0, WINDOW, WINDOW);
         NumberAxis yAxis = new NumberAxis();
-        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis) {
+            @Override
+            protected void layoutPlotChildren() {
+                super.layoutPlotChildren();
+                ObservableList<Series<Number, Number>> data = getData();
+                data.stream().map(Series::getNode).<Path>mapMulti((node, consumer) -> {
+                    if (node instanceof Path path) {
+                        consumer.accept(path);
+                    }
+                }).forEach(this::curve);
+            }
+
+            private void curve(Path path) {
+                ObservableList<PathElement> elements = path.getElements();
+                if (elements.size() > 3) {
+                    curve(elements);
+                }
+            }
+
+            private void curve(ObservableList<PathElement> elements) {
+                int size = elements.size();
+                Point2D[] points = new Point2D[size - 1];
+                for (int i = 0; i < points.length; i++) {
+                    PathElement e = elements.get(i + 1);
+                    if (e instanceof LineTo lineTo) {
+                        points[i] = new Point2D(lineTo.getX(), lineTo.getY());
+                    }
+                }
+                Point2D[] interpolate = new CatmullRom(0.5).interpolate(points, 32);
+                // update
+                for (int i = 1; i < size; i++) {
+                    PathElement e = elements.get(i);
+                    if (e instanceof LineTo lineTo) {
+                        lineTo.setX(interpolate[i - 1].getX());
+                        lineTo.setY(interpolate[i - 1].getY());
+                    }
+                }
+                // add
+                for (int i = 0; i < interpolate.length - size; i++) {
+                    elements.add(new LineTo(interpolate[i + size].getX(), interpolate[i + size].getY()));
+                }
+            }
+        };
         lineChart.setAnimated(false);
         lineChart.setCreateSymbols(false);
         ObservableList<XYChart.Series<Number, Number>> data = lineChart.getData();
