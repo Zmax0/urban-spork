74161e839f55fc7e74f9c62ebb2bdf1ae58659ce
Zmax0 <zhousha104@gmail.com>
2024-07-06 00:37:52 +0800

    feat: add module-info.java

---
+++ urban-spork-client/src/module-info.java
@@ -0,0 +1,12 @@
+module com.urbanspork.client {
+    exports com.urbanspork.client;
+    requires com.urbanspork.common;
+    requires io.netty.buffer;
+    requires io.netty.codec.http;
+    requires io.netty.codec.socks;
+    requires io.netty.codec;
+    requires io.netty.common;
+    requires io.netty.handler;
+    requires io.netty.transport;
+    requires org.slf4j;
+}
