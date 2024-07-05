f1c5c169eb49345369f476f0726bdd2bb6346f27
Zmax0 <zhousha104@gmail.com>
2024-07-06 00:37:52 +0800

    feat: add module-info.java

---
+++ urban-spork-client-gui/src/module-info.java
@@ -0,0 +1,17 @@
+module com.urbanspork.client.gui {
+    requires ch.qos.logback.classic;
+    requires ch.qos.logback.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.jfoenix;
+    requires com.urbanspork.client;
+    requires com.urbanspork.common;
+    requires io.netty.handler;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires org.slf4j;
+
+    exports com.urbanspork.client.gui.console to javafx.graphics, ch.qos.logback.core;
+}
---
+++ urban-spork-server/src/module-info.java
@@ -0,0 +1,12 @@
+module com.urbansport.server {
+    requires com.urbanspork.common;
+    requires io.netty.buffer;
+    requires io.netty.codec.http;
+    requires io.netty.codec.socks;
+    requires io.netty.codec;
+    requires io.netty.common;
+    requires io.netty.handler;
+    requires io.netty.transport;
+    requires org.bouncycastle.lts.prov;
+    requires org.slf4j;
+}
