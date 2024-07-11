227c0528a58feb674788c476563a16766b8984f0
Zmax0 <zhousha104@gmail.com>
2024-07-11 18:12:42 +0800

    fix(gui): fix the issue where the console cannot be opened

--- urban-spork-client-gui/resource/startup.cmd
+++ urban-spork-client-gui/resource/startup.cmd
@@ -1,4 +1,4 @@
 @echo off
 set server="urban-spork-client-gui"
-start javaw --add-opens java.base/java.lang.reflect=com.jfoenix --add-opens javafx.controls/com.sun.javafx.scene.control.behavior=com.jfoenix --add-opens javafx.controls/com.sun.javafx.scene.control=com.jfoenix -jar %server%.jar
+start javaw --module-path lib --add-modules javafx.controls --add-modules com.jfoenix --add-opens java.base/java.lang.reflect=com.jfoenix --add-opens javafx.controls/com.sun.javafx.scene.control.behavior=com.jfoenix --add-opens javafx.controls/com.sun.javafx.scene.control=com.jfoenix -jar %server%.jar
 exit
