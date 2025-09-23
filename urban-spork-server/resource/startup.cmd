8f3b3a817b939d02ea6ca7e5911ed0548f4ced6d
Zmax0 <zhousha104@gmail.com>
2025-09-23 14:17:18 +0800

    refactor(server): update jvm options

--- urban-spork-server/resource/startup.cmd
+++ urban-spork-server/resource/startup.cmd
@@ -2,9 +2,9 @@
 chcp 65001
 set server="urban-spork-server"
 java ^
---add-opens java.base/jdk.internal.misc=ALL-UNNAMED ^
---add-opens java.base/java.nio=ALL-UNNAMED ^
+--enable-native-access=io.netty.common ^
+--add-opens java.base/jdk.internal.misc=io.netty.common ^
+--add-opens java.base/java.nio=io.netty.common ^
 --add-reads io.netty.handler=org.bouncycastle.lts.prov ^
---add-reads io.netty.handler=ALL-UNNAMED ^
--Dio.netty.tryReflectionSetAccessible=true ^
--Xms64m -Xmx256m -jar %server%.jar
+-Dio.netty.eventLoopThreads=8 ^
+-Xms64m -Xmx192m -jar %server%.jar
