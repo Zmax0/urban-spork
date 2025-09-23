ca267983db32a9e34f77a77ab59823bf7e405113
Zmax0 <zhousha104@gmail.com>
2025-09-23 14:17:18 +0800

    refactor(server): update jvm options

--- urban-spork-server/resource/startup.sh
+++ urban-spork-server/resource/startup.sh
@@ -9,12 +9,12 @@ fi
 mkdir -p logs
 touch logs/server.log
 nohup "$JAVA_HOME"/bin/java \
---add-opens java.base/jdk.internal.misc=ALL-UNNAMED \
---add-opens java.base/java.nio=ALL-UNNAMED \
+--enable-native-access=io.netty.common \
+--add-opens java.base/jdk.internal.misc=io.netty.common \
+--add-opens java.base/java.nio=io.netty.common \
 --add-reads io.netty.handler=org.bouncycastle.lts.prov \
---add-reads io.netty.handler=ALL-UNNAMED \
--Dio.netty.tryReflectionSetAccessible=true \
--Xms64m -Xmx256m -jar "${server}.jar" >/dev/null 2>&1 &
+-Dio.netty.eventLoopThreads=8 \
+-Xms32m -Xmx192m -jar "${server}.jar" >/dev/null 2>&1 &
 eval pid="$(pgrep -f ${server})"
 echo "start ${server} pid ${pid}"
 tail -100f logs/server.log
