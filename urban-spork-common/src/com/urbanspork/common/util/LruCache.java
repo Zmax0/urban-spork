d75e079c21a4e5f7a1ef64f3be11484ee48e7470
Zmax0 <zhousha104@gmail.com>
2026-04-24 18:04:09 +0800

    refactor: replace `LruCache` implementation

--- urban-spork-common/src/com/urbanspork/common/util/LruCache.java
+++ urban-spork-common/src/com/urbanspork/common/util/LruCache.java
@@ -1,96 +1,50 @@
 package com.urbanspork.common.util;
 
-import io.netty.util.HashedWheelTimer;
-import io.netty.util.Timeout;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.RemovalCause;
+import com.github.benmanes.caffeine.cache.Scheduler;
 
 import java.time.Duration;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
 public class LruCache<K, V> {
-    final long capacity;
-    final Duration timeToLive;
-    final HashedWheelTimer timer = new HashedWheelTimer(1, TimeUnit.SECONDS);
     final BiConsumer<K, V> afterExpired;
-    final Map<K, Pair<V>> inner = new LinkedHashMap<>(16, 0.75f, true) {
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<K, Pair<V>> eldest) {
-            boolean flag = size() > capacity;
-            if (flag) {
-                K key = eldest.getKey();
-                Pair<V> pair = eldest.getValue();
-                afterExpired.accept(key, pair.value);
-                pair.timeout.cancel();
-            }
-            return flag;
-        }
-    };
+    final Cache<K, V> inner;
 
     public LruCache(long capacity, Duration timeToLive, BiConsumer<K, V> afterExpired) {
-        this.capacity = capacity;
-        this.timeToLive = timeToLive;
         this.afterExpired = afterExpired;
+        this.inner = Caffeine.newBuilder()
+            .maximumSize(capacity)
+            .expireAfterAccess(timeToLive)
+            .scheduler(Scheduler.systemScheduler())
+            .evictionListener((K key, V value, RemovalCause _) -> afterExpired.accept(key, value))
+            .build();
     }
 
     public V computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
-        V old = get(key);
-        if (old == null) {
-            V value = mappingFunction.apply(key);
-            insert(key, value);
-            return value;
-        } else {
-            return old;
-        }
+        return inner.get(key, mappingFunction);
     }
 
     public void insert(K key, V value) {
-        inner.put(key, new Pair<>(value, timer.newTimeout(_ -> expire(key, value), timeToLive.toNanos(), TimeUnit.NANOSECONDS)));
+        inner.put(key, value);
     }
 
     public V get(K key) {
-        Pair<V> pair = inner.get(key);
-        if (pair != null) {
-            pair.timeout.cancel();
-            pair.timeout = timer.newTimeout(_ -> expire(key, pair.value), timeToLive.toNanos(), TimeUnit.NANOSECONDS);
-            return pair.value;
-        } else {
-            return null;
-        }
+        return inner.asMap().get(key);
     }
 
     public V remove(K key) {
-        Pair<V> pair = inner.remove(key);
-        if (pair != null) {
-            pair.timeout.cancel();
-            return pair.value;
-        } else {
-            return null;
-        }
+        return inner.asMap().remove(key);
     }
 
     public void release() {
-        for (Map.Entry<K, Pair<V>> entry : Map.copyOf(inner).entrySet()) {
-            afterExpired.accept(entry.getKey(), entry.getValue().value);
+        for (Map.Entry<K, V> entry : Map.copyOf(inner.asMap()).entrySet()) {
+            afterExpired.accept(entry.getKey(), entry.getValue());
         }
-        timer.stop();
-        inner.clear();
-    }
-
-    private void expire(K key, V value) {
-        afterExpired.accept(key, value);
-        inner.remove(key);
-    }
-
-    static class Pair<V> {
-        final V value;
-        Timeout timeout;
-
-        public Pair(V value, Timeout timeout) {
-            this.value = value;
-            this.timeout = timeout;
-        }
+        inner.invalidateAll();
+        inner.cleanUp();
     }
 }
